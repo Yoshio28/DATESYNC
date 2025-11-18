e632a9cc1996664e314e7bc06f072253a7ce723e
Yoshio28 <yoshiokun8@gmail.com>
2025-11-18 00:08:12 -0600

    implementacion cajones del dashboard inicio

--- lib/views/dashboard_inicio.dart
+++ lib/views/dashboard_inicio.dart
@@ -1,3 +1,7 @@
+import 'package:datesync/views/dashboard_analisis.dart';
+import 'package:datesync/views/dashboard_completo_tarea.dart';
+import 'package:datesync/views/dashboard_tareas.dart';
+import 'package:datesync/views/dashboard_team.dart';
 import 'package:flutter/material.dart';
 import 'package:datesync/model/NavBar.dart';
 
@@ -7,7 +11,62 @@ class Dashboardinicio extends StatelessWidget {
     return Scaffold(
       drawer: Navbar(),
       appBar: AppBar(title: Text('Inicio')),
-      body: Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+
+          children: [
+            _cajones(context, 'Sprints', Colors.deepPurple, DashboardTareas()),
+            _cajones(
+              context,
+              'Calendario',
+              const Color.fromARGB(255, 224, 135, 2),
+              DashboardCompletoTarea(),
+            ),
+            _cajones(
+              context,
+              'Analisis',
+              const Color.fromARGB(255, 0, 136, 64),
+              DashboardAnalisis(),
+            ),
+            _cajones(
+              context,
+              'Equipo de trabajo',
+              const Color.fromARGB(255, 5, 5, 183),
+              DashboardTeam(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _cajones(
+    BuildContext context,
+    String titulo,
+    Color color,
+    Widget pantalla,
+  ) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) => pantalla));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Text(
+            titulo,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+        ),
+      ),
     );
   }
 }
